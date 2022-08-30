90058fa69b6cec3d317c9ab1ee841ba73cdd5aa6
Simon Schlepphorst <dev@simon-schlepphorst.de>
2022-08-30 02:44:01 +0200

    Fix HIP build

--- include/kokkos_defaults.h
+++ include/kokkos_defaults.h
@@ -13,70 +13,74 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Complex.hpp>
 
-namespace MG
-{
+namespace MG {
 
-template<typename T>
+template <typename T>
 using MGComplex = Kokkos::complex<T>;
-using HostExec = Kokkos::Serial;
+using HostExec  = Kokkos::Serial;
 
 #if defined(MG_USE_CUDA)
-  using ExecSpace = Kokkos::Cuda::execution_space;
-  using MemorySpace = Kokkos::Cuda::memory_space;
-  
-  using Layout = Kokkos::LayoutLeft;
-  using GaugeLayout = Kokkos::LayoutLeft;
-  using NeighLayout = Kokkos::LayoutLeft;
+using ExecSpace   = Kokkos::Cuda::execution_space;
+using MemorySpace = Kokkos::Cuda::memory_space;
+
+using Layout      = Kokkos::LayoutLeft;
+using GaugeLayout = Kokkos::LayoutLeft;
+using NeighLayout = Kokkos::LayoutLeft;
 #elif defined(MG_USE_HIP)
-  using ExecSpace = Kokkos::Experimental::HIP::execution_space;
-  using MemorySpace = Kokkos::Experimental::HIP::memory_space;
-  
-  using Layout = Kokkos::LayoutLeft;
-  using GaugeLayout = Kokkos::LayoutLeft;
-  using NeighLayout = Kokkos::LayoutLeft;
+using ExecSpace   = Kokkos::Experimental::HIP::execution_space;
+using MemorySpace = Kokkos::Experimental::HIP::memory_space;
+
+using Layout      = Kokkos::LayoutLeft;
+using GaugeLayout = Kokkos::LayoutLeft;
+using NeighLayout = Kokkos::LayoutLeft;
 #elif defined(MG_USE_OPENMPTARGET)
-  using ExecSpace = Kokkos::Experimental::OpenMPTarget::execution_space;
-  using MemorySpace = Kokkos::Experimental::OpenMPTarget::memory_space;
-  using Layout = Kokkos::LayoutLeft;
-  using GaugeLayout = Kokkos::LayoutLeft;
-  using NeighLayout = Kokkos::LayoutLeft;
+using ExecSpace        = Kokkos::Experimental::OpenMPTarget::execution_space;
+using MemorySpace      = Kokkos::Experimental::OpenMPTarget::memory_space;
+using Layout           = Kokkos::LayoutLeft;
+using GaugeLayout      = Kokkos::LayoutLeft;
+using NeighLayout      = Kokkos::LayoutLeft;
 #else
 
-  using ExecSpace = Kokkos::OpenMP::execution_space;
-  using MemorySpace = Kokkos::OpenMP::memory_space;
-  using Layout = Kokkos::LayoutRight;
-  using GaugeLayout = Kokkos::LayoutRight;
-  using NeighLayout = Kokkos::OpenMP::array_layout;
+using ExecSpace   = Kokkos::OpenMP::execution_space;
+using MemorySpace = Kokkos::OpenMP::memory_space;
+using Layout      = Kokkos::LayoutRight;
+using GaugeLayout = Kokkos::LayoutRight;
+using NeighLayout = Kokkos::OpenMP::array_layout;
 #endif
 
 #if defined(MG_USE_CUDA) || defined(MG_USE_OPENMPTARGET)
-using ThreadExecPolicy =  Kokkos::TeamPolicy<ExecSpace,Kokkos::LaunchBounds<128,1>>;
+using ThreadExecPolicy =
+    Kokkos::TeamPolicy<ExecSpace, Kokkos::LaunchBounds<128, 1>>;
 using SimpleRange = Kokkos::RangePolicy<ExecSpace>;
 #elif defined(MG_USE_HIP)
-using ThreadExecPolicy =  Kokkos::TeamPolicy<ExecSpace,Kokkos::LaunchBounds<256,1>>;
-using SimpleRange = Kokkos::RangePolicy<ExecSpace,Kokkos::LaunchBounds<256,1>>;
+using ThreadExecPolicy =
+    Kokkos::TeamPolicy<ExecSpace, Kokkos::LaunchBounds<256, 1>>;
+using SimpleRange =
+    Kokkos::RangePolicy<ExecSpace, Kokkos::LaunchBounds<256, 1>>;
 #else
 using ThreadExecPolicy = Kokkos::TeamPolicy<ExecSpace>;
-using SimpleRange = Kokkos::RangePolicy<ExecSpace>;
+using SimpleRange      = Kokkos::RangePolicy<ExecSpace>;
 #endif
 
-
-#if defined(MG_USE_CUDA) || defined(MG_USE_AVX512) || defined(MG_USE_AVX2) || defined(MG_USE_SVE512)
-using TeamHandle =  ThreadExecPolicy::member_type;
-using VectorPolicy = Kokkos::Impl::ThreadVectorRangeBoundariesStruct<int,TeamHandle>;
+#if defined(MG_USE_CUDA) || defined(MG_USE_AVX512) || defined(MG_USE_AVX2) || \
+    defined(MG_USE_SVE512) || defined(MG_USE_HIP)
+using TeamHandle = ThreadExecPolicy::member_type;
+using VectorPolicy =
+    Kokkos::Impl::ThreadVectorRangeBoundariesStruct<int, TeamHandle>;
 #endif
 
-#if defined(MG_USE_CUDA)
-  // Try an N-dimensional threading policy for cache blocking
- using MDPolicy =  Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<4,
-   Kokkos::Experimental::Iterate::Left,Kokkos::Experimental::Iterate::Left>, Kokkos::LaunchBounds<512,1>>;
+#if defined(MG_USE_CUDA) || defined(MG_USE_HIP)
+// Try an N-dimensional threading policy for cache blocking
+using MDPolicy = Kokkos::MDRangePolicy<
+    Kokkos::Rank<4, Kokkos::Iterate::Left, Kokkos::Iterate::Left>,
+    Kokkos::LaunchBounds<512, 1>>;
 
 #else
- // Try an N-dimensional threading policy for cache blocking
- using MDPolicy =  Kokkos::MDRangePolicy<Kokkos::Rank<4,
-   Kokkos::Iterate::Left,Kokkos::Iterate::Left>>;
+// Try an N-dimensional threading policy for cache blocking
+using MDPolicy = Kokkos::MDRangePolicy<
+    Kokkos::Rank<4, Kokkos::Iterate::Left, Kokkos::Iterate::Left>>;
 
-#endif  
-}
+#endif
+}  // namespace MG
 
 #endif /* TEST_KOKKOS_KOKKOS_DEFAULTS_H_ */
